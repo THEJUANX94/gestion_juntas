77c1f286768716da17ddd3c3e6d8924533ef98d1
Sam Vargas <samu.davargas@gmail.com>
2025-12-13 18:28:45 -0500

    confio

--- backend/src/config/asociacionesBD.js
+++ backend/src/config/asociacionesBD.js
@@ -58,7 +58,11 @@ export const Asociaciones = () => {
     // RELACIONES PARA JUNTA
     // ========================================
     Junta.belongsTo(Lugar, { foreignKey: "idmunicipio" });
-    Junta.belongsTo(TipoJunta, { foreignKey: "tipojunta" });
+    Junta.belongsTo(TipoJunta, {
+        foreignKey: "tipojunta",
+        targetKey: "idtipojuntas",
+        as: "TipoJunta"
+    });
     Junta.belongsTo(Institucion, { foreignKey: "idinstitucion" });
     Junta.belongsTo(Reconocida, {
         foreignKey: "IDReconocida",
