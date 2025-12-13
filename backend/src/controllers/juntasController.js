855ebadd271ce19a7e249c3d1e2197f660d59a08
Sam Vargas <samu.davargas@gmail.com>
2025-12-13 18:21:16 -0500

    mirar

--- backend/src/controllers/juntasController.js
+++ backend/src/controllers/juntasController.js
@@ -610,6 +610,14 @@ export const exportarJuntasExcel = async (req, res) => {
       order: [["RazonSocial", "ASC"]],
     });
 
+    console.log(
+    juntas.map(j => ({
+      IDJunta: j.IDJunta,
+      FK_tipojunta: j.tipojunta,
+      TipoJunta: j.tipoJunta
+    }))
+  );
+
     const workbook = new ExcelJS.Workbook();
     const sheet = workbook.addWorksheet("Juntas");
 
