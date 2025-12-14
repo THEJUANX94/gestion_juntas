714f3f66450e7471398279ada1cc22d3d37b9c59
Sam Vargas <samu.davargas@gmail.com>
2025-12-14 14:01:29 -0500

    mas gorgeus

--- frontend/src/pages/ListarJuntas.jsx
+++ frontend/src/pages/ListarJuntas.jsx
@@ -336,7 +336,7 @@ export default function ListarJuntas() {
                     </div>
                 </div>
 
-                <div className="mt-4">
+                <div className="mt-4 ml-auto">
                     <button
                         onClick={descargarExcel}
                         className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition"
