c5d1b760e3d1c17b93734d86375c5be6a21eada1
thejuanx94 <sebastianmn03@hotmail.com>
2026-01-21 20:01:17 -0500

    Import faltante

--- backend/src/controllers/usuarioController.js
+++ backend/src/controllers/usuarioController.js
@@ -6,6 +6,7 @@ import { Op } from "sequelize";
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcryptjs";
 import { Credenciales } from "../model/credencialesModel.js";
+import { TipoDocumento } from '../models/TipoDocumento.js';
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
