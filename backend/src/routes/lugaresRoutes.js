40bc95ea382c6fb5f0fc0118371b0987433839ec
THEJUANX94 <sebastianmn03@hotmail.com>
2026-01-23 11:12:00 -0500

    Cierto

--- backend/src/routes/lugaresRoutes.js
+++ backend/src/routes/lugaresRoutes.js
@@ -16,6 +16,6 @@ router.get("/:idlugar", verificarAuth, obtenerLugarPorId);
 router.delete("/:idlugar", verificarAuth, eliminarLugar);
 router.post("/crearlugar", verificarAuth, crearLugar);
 router.patch("/:idlugar/estado", verificarAuth, cambiarEstadoLugar);
-router.get("/:departamento", verificarAuth, obtenerMunicipiosPorDepartamento)
+router.get("/municipios", verificarAuth, obtenerMunicipiosPorDepartamento)
 
 export default router;
