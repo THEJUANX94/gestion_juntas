5b1077492837a67e6cda683aa1b26db77d5c6ee2
THEJUANX94 <sebastianmn03@hotmail.com>
2026-01-27 15:02:32 -0500

    deptoid

--- frontend/src/pages/EditarMandatario.jsx
+++ frontend/src/pages/EditarMandatario.jsx
@@ -60,6 +60,9 @@ export default function EditarMandatario() {
         const resMand = await fetch(import.meta.env.VITE_PATH + `/mandatario/${id}/${documento}`);
         const mand = await resMand.json();
 
+        const municipioInfo = dataLugares.find(l => l.IDLugar === mand.expedido);
+        const deptoId = municipioInfo ? municipioInfo.IDOtroLugar : "";
+
         setFormData({
           documento: mand.documento,
           tipoDocumento: mand.tipoDocumento,
@@ -211,7 +214,7 @@ export default function EditarMandatario() {
             <div>
               <h3 className="text-lg font-bold text-gray-800 mb-4 pb-2 border-b-2 border-[#64AF59]">Datos Personales</h3>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-4">
-                
+
                 {/* 1. Selector de Departamentos */}
                 <Select
                   label="Departamento"
