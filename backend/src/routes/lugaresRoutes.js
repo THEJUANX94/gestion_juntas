fbafda8257052c7fe79898bca93a7eaea6f75db8
thejuanx94 <sebastianmn03@hotmail.com>
2026-02-12 23:00:19 -0500

    Prueba de roles

--- backend/src/routes/lugaresRoutes.js
+++ backend/src/routes/lugaresRoutes.js
@@ -12,8 +12,8 @@ import { ROLES } from "../config/roles.js";
 
 const router = Router();
 
-router.get("/", verificarAuth, verificarRol([ROLES.ADMIN, ROLES.AUXILIAR, ROLES.CONSULTA]), obtenerLugares);
-router.get("/municipios", verificarAuth, verificarRol([ROLES.ADMIN, ROLES.AUXILIAR, ROLES.CONSULTA]), obtenerMunicipiosPorDepartamento)
+router.get("/", verificarAuth, obtenerLugares);
+router.get("/municipios", verificarAuth, obtenerMunicipiosPorDepartamento)
 router.get("/:idlugar", verificarAuth, verificarRol([ROLES.ADMIN, ROLES.AUXILIAR, ROLES.CONSULTA]), obtenerLugarPorId);
 router.delete("/:idlugar", verificarAuth, verificarRol([ROLES.ADMIN]), eliminarLugar);
 router.post("/crearlugar", verificarAuth, verificarRol([ROLES.ADMIN, ROLES.AUXILIAR]), crearLugar);
--- backend/src/routes/tipoJuntaRoutes.js
+++ backend/src/routes/tipoJuntaRoutes.js
@@ -11,7 +11,7 @@ import { ROLES } from "../config/roles.js";
 
 const router = Router();
 
-router.get("/",  verificarAuth, verificarRol([ROLES.ADMIN, ROLES.AUXILIAR, ROLES.CONSULTA]), obtenerTiposJunta);
+router.get("/",  verificarAuth, obtenerTiposJunta);
 router.get("/:idtipojunta",  verificarAuth, verificarRol([ROLES.ADMIN, ROLES.AUXILIAR, ROLES.CONSULTA]), obtenerTipoJuntaPorId);
 router.delete("/:idtipojunta", verificarAuth, verificarRol([ROLES.ADMIN]), eliminarTipoJunta);
 router.post("/creartipojunta", verificarAuth, verificarRol([ROLES.ADMIN]), crearTipoJunta);
