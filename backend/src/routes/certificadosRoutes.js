4acf04b6d850cf4860eb9d77ce1df2e4a0d04538
thejuanx94 <sebastianmn03@hotmail.com>
2026-02-12 23:24:58 -0500

    Cambio de ruta

--- backend/src/routes/certificadosRoutes.js
+++ backend/src/routes/certificadosRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { crearCertificado, validarCertificado } from "../controllers/certificadosController.js";
+import { crearCertificado, enviarAutoresolutorio, validarCertificado } from "../controllers/certificadosController.js";
 import { verificarAuth, verificarRol } from "../utils/authMiddleware.js";
 import { ROLES } from "../config/roles.js";
 
 const router = Router();
 
 router.post('/', verificarAuth, verificarRol([ROLES.ADMIN, ROLES.AUXILIAR]), crearCertificado);
-router.get('/solicitar', crearCertificado);
+router.get('/solicitar', enviarAutoresolutorio);
 router.get('/validar/:IDCertificado', validarCertificado);
 
 export default router;
