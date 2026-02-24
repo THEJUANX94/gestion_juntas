4ae89628c539811cbadb86decea6e9b0e771a812
THEJUANX94 <sebastianmn03@hotmail.com>
2026-02-24 09:08:16 -0500

    clg de comprobacion

--- backend/src/utils/authMiddleware.js
+++ backend/src/utils/authMiddleware.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 export const verificarAuth = (req, res, next) => {
-    const token = req.cookies.auth_token;
+  const token = req.cookies.auth_token;
 
-    if (!token) {
-        return res.status(401).json({ error: 'Acceso denegado. (Token no encontrado)' }); 
-    }
-    
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); 
-        req.usuario = decoded; 
-        
-        console.log(`Token verificado. Usuario ID: ${decoded.id}`); 
-        
-        next(); 
-    } catch (err) {
-        return res.status(401).json({ error: 'Token inválido o expirado.' });
-    }
+  if (!token) {
+    return res.status(401).json({ error: 'Acceso denegado. (Token no encontrado)' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuario = decoded;
+
+    console.log(`Token verificado. Usuario ID: ${decoded.id}`);
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token inválido o expirado.' });
+  }
 };
 
 export const verificarRol = (rolesPermitidos) => {
@@ -24,8 +24,12 @@ export const verificarRol = (rolesPermitidos) => {
     if (!req.usuario) {
       return res.status(401).json({ error: 'No autenticado.' });
     }
+
+    console.log("Roles permitidos:", rolesPermitidos);
+    console.log("Rol del usuario haciendo la petición:", req.usuario.rol);
+    console.log("Tipo de dato del rol:", typeof req.usuario.rol);
     if (!rolesPermitidos.includes(req.usuario.rol)) {
-       return res.status(403).json({ error: 'No tienes permisos para realizar esta acción.' });
+      return res.status(403).json({ error: 'No tienes permisos para realizar esta acción.' });
     }
 
     next();
