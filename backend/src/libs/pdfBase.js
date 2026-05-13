405577dcc7e0fe02943b5ef05749efc5903a2310
Thejuanx9 <sebastianmn03@hotmail.com>
2026-05-13 16:45:25 -0500

    aa

--- backend/src/libs/pdfBase.js
+++ backend/src/libs/pdfBase.js
@@ -120,7 +120,7 @@ export const addPDFHeader = async (doc, datosCertificado) => {
 
   // Logo
   if (resources.base64Logo) {
-    doc.addImage(resources.base64Logo, "PNG", margenIzq, 8, 45, 25);
+    doc.addImage(resources.base64Logo, "PNG", margenIzq, 8, 40, 28);
   }
 
   // QR a la derecha (omitir en modo preview)
